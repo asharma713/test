cebfda9f9f4bad18b766a524deb2f1a8067d39d6
Amit Sharma <amitsharma@mac.com>
2015-12-18 09:54:22 -0600

    Initial Commit

---
+++ test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by Amit Sharma on 12/18/15.
+//  Copyright © 2015 Amit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by Amit Sharma on 12/18/15.
+//  Copyright © 2015 Amit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
